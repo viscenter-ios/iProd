7ff7076eeafe3fdf4a4590c4580b55f55888c579
Justin Proffitt <justin.proffitt@uky.edu>
2011-06-02 20:50:45 +0000

    Commit the files for the about view controller.

---
+++ Classes/AboutVC.h
@@ -0,0 +1,18 @@
+//
+//  AboutVC.h
+//  iProd2
+//
+//  Created by Justin Proffitt on 6/2/11.
+//  Copyright 2011 University Of Kentucky. All rights reserved.
+//
+//This class is used to display the about information for the app. The goToMain function
+//is the only function and goes to main.
+
+#import <UIKit/UIKit.h>
+
+
+@interface AboutVC : UIViewController {
+    
+}
+-(IBAction) goToMain;
+@end
---
+++ Classes/AboutVC.m
@@ -0,0 +1,61 @@
+//
+//  AboutVC.m
+//  iProd2
+//
+//  Created by Justin Proffitt on 6/2/11.
+//  Copyright 2011 University Of Kentucky. All rights reserved.
+//
+
+#import "AboutVC.h"
+
+
+@implementation AboutVC
+
+-(IBAction) goToMain
+{
+  [self.navigationController popToRootViewControllerAnimated:YES];
+}
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
